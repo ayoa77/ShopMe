5d6f4a772a0977177866767bad71d5aad7598c1c
ayoa77 <ayodeleamadi@gmail.com>
2017-03-27 03:42:31 +0800

    added payment functionality for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'braintree/new'
+  post 'braintree/checkout'
+
   resources :posts
   controller :sessions do
     get 'login' => :new
@@ -6,7 +9,6 @@ Rails.application.routes.draw do
     delete 'logout' => :destroy
   end
   resources :users
-
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   root 'welcome#home'
