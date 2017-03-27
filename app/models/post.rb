28a358e98b629746e5343bbbbd0ec3e85658565e
ayoa77 <ayodeleamadi@gmail.com>
2017-03-27 13:14:35 +0800

    search function added for Users

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,14 @@
 class Post < ApplicationRecord
+  include PgSearch
   mount_uploader :post_photo, PhotoUploader
   belongs_to :user, dependent: :destroy
   validates_presence_of :post_photo, :description, :price
+  pg_search_scope :search_posts, :against => :description,
+                 :using => {
+   tsearch:    {dictionary: 'english', prefix: true},
+   trigram:    {threshold:  0.3},
+   dmetaphone: {}
+ }
+
+
 end
